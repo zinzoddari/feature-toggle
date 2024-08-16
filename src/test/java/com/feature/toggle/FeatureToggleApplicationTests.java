e411c2d225272c4e8f75145f546faafc5e0f6f82
zinzo <zinzoddari@gmail.com>
2024-08-16 23:05:58 +0900

    init

---
+++ src/test/java/com/feature/toggle/FeatureToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feature.toggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
